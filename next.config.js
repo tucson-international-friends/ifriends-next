14faf57c7d65044eeda8808d33f5b8753774405d
Youhao Wei <wei@youhao.me>
2019-03-26 00:54:02 -0700

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const path = require("path");
 
 module.exports = withSass({
 	cssModules: true,
+	cssLoaderOptions: {
+		getLocalIdent: (loaderContext, localIdentName, localName, options) => {
+			const fileName = path.basename(loaderContext.resourcePath);
+			if (
+				fileName.indexOf("global.scss") !== -1
+				|| fileName.indexOf("node_modules") !== -1
+			) {
+				return localName;
+			}
+			const name = fileName.replace(/\.[^/.]+$/, "");
+			return `${name}__${localName}`;
+		}
+	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
