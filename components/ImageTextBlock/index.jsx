e33bbb15add111feb178d16a3969cd58fd8dc4c6
Youhao Wei <wei@youhao.me>
2019-03-29 00:09:41 -0700

    Update styles
    enchanced styles for imageTextBlock and overhaul styles

--- components/ImageTextBlock/index.jsx
+++ components/ImageTextBlock/index.jsx
@@ -1,37 +1,38 @@
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col, Image, Button } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
-import classNames from "classnames";
+import classNames from "classnames/bind";
+import Link from "next/link";
 import styles from "./styles.scss";
+
+const cx = classNames.bind(styles);
 
 const ImageTextBlock = ({
 	title,
 	content,
 	image,
-	revert,
+	invert,
 	color = "dark",
 	readMore
-}) => {
-	if (revert) {
-		color = "light";
-	}
-	return (
-		<Row className={styles.container}>
-			<Col
-				md={{ span: 6, offset: revert ? 6 : 0 }}
-				className={classNames(styles.imageCol, styles[color])}>
-				<Image src={image} />
-			</Col>
-			<div className={classNames(styles.backdrop, styles[color])} />
-			<Col
-				md={{ span: 6, offset: revert ? 0 : 6 }}
-				className={classNames(styles.textCol, styles[color])}>
-				<div className={styles.textContainer}>
-					<h2>{title}</h2>
-					<ReactMarkdown source={content} />
-				</div>
-			</Col>
-		</Row>
-	);
-};
+}) => (
+	<Row className="container">
+		<Col
+			md={{ span: 6, offset: invert ? 6 : 0 }}
+			className={cx("imageCol", color)}>
+			<Image src={image} />
+		</Col>
+		<div className={cx("backdrop", color, { invert })} />
+		<Col
+			md={{ span: 6, offset: invert ? 0 : 6 }}
+			className={cx("textCol", styles[color])}>
+			<div className={cx("textContainer")}>
+				<h2>{title}</h2>
+				<ReactMarkdown source={content} />
+				<Link href={readMore.href}>
+					<Button variant="outline-light">Learn More</Button>
+				</Link>
+			</div>
+		</Col>
+	</Row>
+);
 
 export default ImageTextBlock;
