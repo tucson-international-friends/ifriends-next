70a48bddc8c440147a0c99c633533c2a6f6eb795
Youhao Wei <wei@youhao.me>
2019-09-18 14:19:26 -0700

    fix a problem

--- layout/main.jsx
+++ layout/main.jsx
@@ -11,24 +11,33 @@ import "../styles/global/main.scss";
 import { useEffect } from "react";
 import { useGlobal } from "reactn";
 import firebase from "../lib/firebase";
+import { ACTIONS } from "../components/Action";
 
 addReactNDevTools();
 
-
-const MainLayout = ({ children }) => (
-	<React.Fragment>
-		<Meta />
-		<Navbar
-			navItems={navItems.map(value => getPageByName(value))}
-			navActions={[
-				// { type: ACTIONS.LOGIN },
-				{ type: ACTIONS.SIGNUP }
-			]} />
-		<div
-			className="page"
-		>{children}
-		</div>
-		<Footer menuItems={footerNavItems.map(value => getPageByName(value))} />
-	</React.Fragment>
-);
+const MainLayout = ({ children }) => {
+	if (firebase.auth().currentUser)
+		useEffect(() => {
+			firebase
+				.auth()
+				.currentUser.getIdToken(true)
+				.then(token => {
+					console.log(token);
+				});
+		}, []);
+	return (
+		<>
+			<Meta />
+			<Navbar
+				navItems={navItems.map(value => getPageByName(value))}
+				navActions={[
+					// { type: ACTIONS.LOGIN },
+					{ type: ACTIONS.SIGNUP }
+				]}
+			/>
+			<div className="page">{children}</div>
+			<Footer menuItems={footerNavItems.map(value => getPageByName(value))} />
+		</>
+	);
+};
 export default MainLayout;
