4dda70d681f02ec1780e5500d5aaf2f404564d34
Youhao Wei <wei@youhao.me>
2020-08-29 15:27:42 -0700

    update packages and change what we do link

--- pages/index.jsx
+++ pages/index.jsx
@@ -47,7 +47,7 @@ export default class Index extends Component {
 						actions: [
 							{
 								type: ACTIONS.READMORE,
-								href: "/hosts"
+								href: "/about"
 							}
 						]
 					}
