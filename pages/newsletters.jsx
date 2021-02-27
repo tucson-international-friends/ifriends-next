911964981ae3fb53f7afad6180c9f179d830b69b
Youhao Wei <wei@youhao.me>
2021-02-27 13:33:05 -0700

    Add archive newsletters

--- pages/newsletters.jsx
+++ pages/newsletters.jsx
@@ -1,44 +1,53 @@
 import React from "react";
-import MainLayout from "../layout/main";
 import { Container } from "react-bootstrap";
+import MainLayout from "../layout/main";
 import { getStorageMediaUrl } from "../lib/image";
-import useWindowSize from "../hooks/useWindowSize";
 
 const newsLetters = [{
-	title: 'Summer 2020',
-	path: 'SM2020.pdf'
+	title: "Summer 2020",
+	path: "SM2020.pdf"
 }, {
 	title: "Spring 2020",
-	path: 'SP2020.pdf'
-}];
+	path: "SP2020.pdf"
+}, {
+	title: "Fall 2016",
+	path: "IF.Newsletter.2016.pdf"
+}, {
+	title: "Fall 2015",
+	path: "IF.Newsletter.2015.pdf"
+}, { title: "Fall 2013", path: "IF.Newsletter.2013.pdf" },
+{ title: "Fall 2012", path: "If.Newsletter.2012.pdf" }];
 
 
 const NewsLetters = () => {
-	const { width, height } = useWindowSize();
 	const latest = newsLetters[0];
 	const past = newsLetters.slice(1);
-	return <MainLayout>
-		<Container style={{
-			paddingTop: "10em",
-			paddingBottom: "10em"
-		}}>
+	return (
+		<MainLayout>
+			<Container style={{
+				paddingTop: "10em",
+				paddingBottom: "10em"
+			}}>
 
-			<div className="py-4">
-				<h2>Latest - {latest.title}</h2>
-				<a className="btn btn-primary" target="_blank" href={getStorageMediaUrl(`newsletters/${latest.path}`)} >View PDF</a>
+				<div className="py-4">
+					<h2>Latest - {latest.title}</h2>
+					<a rel="noreferrer" className="btn btn-primary" target="_blank" href={getStorageMediaUrl(`newsletters/${latest.path}`)}>View PDF</a>
 
-			</div>
-			<iframe width="100%" style={{ height: "75vh" }} src={getStorageMediaUrl(`newsletters/${latest.path}`)} />
-			<div className="py-4">
-				<h2>Archive</h2>
-				<ul>
-					{past.map(({ path, title }) => <li key={path}>
-						{title} - <a href={getStorageMediaUrl(`newsletters/${path}`)}>view</a>
-					</li>)}
-				</ul>
-			</div>
-		</Container>
-	</MainLayout>;
+				</div>
+				<iframe title="latest newsletter" width="100%" style={{ height: "75vh" }} src={getStorageMediaUrl(`newsletters/${latest.path}`)} />
+				<div className="py-4">
+					<h2>Archive</h2>
+					<ul>
+						{past.map(({ path, title }) => (
+							<li key={path}>
+								{title} - <a href={getStorageMediaUrl(`newsletters/${path}`)}>view</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</Container>
+		</MainLayout>
+	);
 };
 
 export default NewsLetters;
